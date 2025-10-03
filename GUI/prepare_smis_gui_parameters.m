3ee8ed677402b5bfe3de2538996fbcef946dd539
DominiqueBourgeois <dominique.bourgeois@ibs.fr>
2025-10-03 13:52:35 +0200

    Fixed bug
    Fixed bug with "sample_z_coverslip" not set when starting a simulation from scratch

--- GUI/prepare_smis_gui_parameters.m
+++ GUI/prepare_smis_gui_parameters.m
@@ -112,7 +112,8 @@ smis_gui_parameters.obj_and_psf = struct(...
     'psf_astigmatism_y_save', [-2.50, 1.25], ...; % Backup values for Astigmatism in Y-dimension + 4rth order correction
     'psf_astigmatism_x', [0,0], ...; % Astigmatism in X-dimension + 4rth order correction
     'psf_astigmatism_y', [0,0], ...; % Astigmatism in Y-dimension + 4rth order correction
-    'psf_ch1_only', 0 ...; % Set to 1 if astigmatism to be applied only on channel 1 in two-channel experiments
+    'psf_ch1_only', 0, ...; % Set to 1 if astigmatism to be applied only on channel 1 in two-channel experiments
+    'sample_z_coverslip', 0 ...; % Set to 1 if astigmatism to be applied only on channel 1 in two-channel experiments
     );
 
 % Define default lasers
